145c4720be95bde04cb5d2e9360fa192eb6e2bf9
Alperen Kavun <alperen@halici.com.tr>
2012-11-14 17:37:09 +0200

    Added navigation controllers for each tab

---
+++ CHP-Mobil/CHPBizKimizNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  CHPBizKimizNavigationController.h
+//  CHP-Mobil
+//
+//  Created by Alperen Kavun on 14.11.2012.
+//  Copyright (c) 2012 Halıcı. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHPBizKimizNavigationController : UINavigationController
+
+@end
---
+++ CHP-Mobil/CHPYoneticilerNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  CHPYoneticilerNavigationController.m
+//  CHP-Mobil
+//
+//  Created by Alperen Kavun on 14.11.2012.
+//  Copyright (c) 2012 Halıcı. All rights reserved.
+//
+
+#import "CHPYoneticilerNavigationController.h"
+
+@interface CHPYoneticilerNavigationController ()
+
+@end
+
+@implementation CHPYoneticilerNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
