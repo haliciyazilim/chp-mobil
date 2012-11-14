582d1eac4149b7a0ecc985d0cf28dd5a29d844a0
Eren Halici <eren@halici.com.tr>
2012-11-14 16:44:06 +0200

    Added MKNetworkKit and APIManager

---
+++ CHP-Mobil/APIManager.h
@@ -0,0 +1,13 @@
+//
+//  APIManager.h
+//  CHP-Mobil
+//
+//  Created by Eren Halici on 14.11.2012.
+//  Copyright (c) 2012 Halıcı. All rights reserved.
+//
+
+#import "MKNetworkEngine.h"
+
+@interface APIManager : MKNetworkEngine
+
+@end
---
+++ CHP-Mobil/APIManager.m
@@ -0,0 +1,13 @@
+//
+//  APIManager.m
+//  CHP-Mobil
+//
+//  Created by Eren Halici on 14.11.2012.
+//  Copyright (c) 2012 Halıcı. All rights reserved.
+//
+
+#import "APIManager.h"
+
+@implementation APIManager
+
+@end
