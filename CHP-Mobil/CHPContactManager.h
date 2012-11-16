1a375807d133783e0f4c3d473f90c7a2190e0958
Eren Halici <eren@halici.com.tr>
2012-11-16 16:57:17 +0200

    Added CHPContactManager class

---
+++ CHP-Mobil/CHPContactManager.h
@@ -0,0 +1,13 @@
+//
+//  CHPContactManager.h
+//  CHP-Mobil
+//
+//  Created by Eren Halici on 16.11.2012.
+//  Copyright (c) 2012 Halıcı. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHPContactManager : NSObject
+
+@end
