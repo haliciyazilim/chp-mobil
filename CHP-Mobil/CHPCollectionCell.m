b3ed0e0c414dadc547a83c068b2b7b60444e29a9
Eren Halici <eren@halici.com.tr>
2012-11-19 18:08:48 +0200

    Added untracked files

---
+++ CHP-Mobil/CHPCollectionCell.m
@@ -0,0 +1,31 @@
+//
+//  CHPCollectionCell.m
+//  CHP-Mobil
+//
+//  Created by Yunus Eren Guzel on 11/19/12.
+//  Copyright (c) 2012 Halıcı. All rights reserved.
+//
+
+#import "CHPCollectionCell.h"
+
+@implementation CHPCollectionCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ CHP-Mobil/CHPHaberDetailViewController.m
@@ -0,0 +1,31 @@
+//
+//  CHPHaberDetailViewController.m
+//  CHP-Mobil
+//
+//  Created by Yunus Eren Guzel on 11/19/12.
+//  Copyright (c) 2012 Halıcı. All rights reserved.
+//
+
+#import "CHPHaberDetailViewController.h"
+
+@implementation CHPHaberDetailViewController
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
