61df2048364319551be7c153a7a20b47948c252b
Alperen Kavun <alperen@halici.com.tr>
2012-11-23 10:34:42 +0200

    Added: searchTableViewCell class, subclassed cell and cell's style is now subtitle for search result table

---
+++ CHP-Mobil/CHPSearchTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CHPSearchTableViewCell.h
+//  CHP-Mobil
+//
+//  Created by Alperen Kavun on 22.11.2012.
+//  Copyright (c) 2012 Halıcı. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHPSearchTableViewCell : UITableViewCell
+
+@end
