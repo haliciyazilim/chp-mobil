7bbb22a988205ba18dbdd8305cf6c45fd6857fb6
Alperen Kavun <alperen@halici.com.tr>
2013-03-04 15:08:14 +0200

    iOS: added new classes

---
+++ iOS/CHP-Mobil/CHPPerson.m
@@ -0,0 +1,27 @@
+//
+//  CHPPerson.m
+//  CHP-Mobil
+//
+//  Created by Alperen Kavun on 01.03.2013.
+//  Copyright (c) 2013 Halıcı. All rights reserved.
+//
+
+#import "CHPPerson.h"
+#import "CHPContact.h"
+
+@implementation CHPPerson
+
++ (CHPPerson *)CHPPersonFromDictionary:(NSDictionary *)personDictionary {
+    return [[CHPPerson alloc] initFromDictionary:personDictionary];
+}
+
+- (id) initFromDictionary:(NSDictionary *)personDictionary {
+    if (self = [super init]) {
+        CHPContact *contact = [CHPContact contactFromDictionary:personDictionary];
+        self.contact = [CHPContact addContact:contact];
+    }
+    
+    return self;
+}
+
+@end
